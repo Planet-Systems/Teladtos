3b4ab3e94a4cd642c41ab31800b20f7c84c39db3
fredrickroyal <kfredrick35@gmail.com>
2022-01-06 10:05:01 +0300

    added multi-tenancy

---
+++ src/main/java/com/planetsystems/tela/dto/exports/DataExportRequestDTO.java
@@ -0,0 +1,54 @@
+package com.planetsystems.tela.dto.exports;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class DataExportRequestDTO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private String json;
+	private List<String> columns;
+	private String fileName;
+	private String refId;
+
+	public DataExportRequestDTO() {
+
+	}
+
+	public String getJson() {
+		return json;
+	}
+
+	public void setJson(String json) {
+		this.json = json;
+	}
+
+	public List<String> getColumns() {
+		return columns;
+	}
+
+	public void setColumns(List<String> columns) {
+		this.columns = columns;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getRefId() {
+		return refId;
+	}
+
+	public void setRefId(String refId) {
+		this.refId = refId;
+	}
+
+}
---
+++ src/main/java/com/planetsystems/tela/dto/exports/SchoolStaffExportDTO.java
@@ -0,0 +1,114 @@
+package com.planetsystems.tela.dto.exports;
+
+import java.io.Serializable;
+
+public class SchoolStaffExportDTO implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String staffCode;
+	private String role;
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	private String email;
+	private String nationalId;
+	private String gender;
+	private String nameAbbrev;
+	private String school;
+	private String district;
+
+	public SchoolStaffExportDTO() {
+
+	}
+
+	public String getStaffCode() {
+		return staffCode;
+	}
+
+	public void setStaffCode(String staffCode) {
+		this.staffCode = staffCode;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNationalId() {
+		return nationalId;
+	}
+
+	public void setNationalId(String nationalId) {
+		this.nationalId = nationalId;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getNameAbbrev() {
+		return nameAbbrev;
+	}
+
+	public void setNameAbbrev(String nameAbbrev) {
+		this.nameAbbrev = nameAbbrev;
+	}
+
+	public String getSchool() {
+		return school;
+	}
+
+	public void setSchool(String school) {
+		this.school = school;
+	}
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+}
